49f5a068a7cef378209c5e6a50739d97ae6f07d6
Vadim000009 <tuukvadim@live.com>
2021-04-29 20:05:55 +0300

    Первые наработки работающей программы

--- collectionsOfLists/models.py
+++ collectionsOfLists/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+# Create your models here. Spok bro
+class Student(models.Model):
+    fstName = models.CharField("Student name", max_length=40)
+    secName = models.CharField("Student surname", max_length=80)
+    patronymic = models.CharField("Student patronymic", max_length=50)
+    averageMark = models.FloatField("Average mark")
+
+    def __str__(self):
+        return str(self.fstName) + str(self.secName) + str(self.patronymic) + str(self.averageMark)
--- collectionsOfLists/views.py
+++ collectionsOfLists/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+# Create your views here. Ok
+def listOfStudents(request):
+    return HttpResponse("OK")
+
+
+def getAllStudents(request):
+    return HttpResponse("OK")
+
+
+def addStudentsToList(request):
+    return HttpResponse("OK")
+
