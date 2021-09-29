5b52c8abd75d627d3a41613085598243bb2248e9
Vadim000009 <tuukvadim@live.com>
2021-09-29 17:09:39 +0300

    Выполнение моей любимой ДЗшочки

--- DjangoUnchained/DjangoUnchained/settings.py
+++ DjangoUnchained/DjangoUnchained/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'XMLCollection',
+    'django_extensions',
+    'django.contrib.admindocs',
 ]
 
 MIDDLEWARE = [
